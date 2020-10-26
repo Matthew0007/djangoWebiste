0cb5457f4bdb0ebb261ef70f55094537cae5ba70
Taiwo Obadare <taiwo.obadare@gmail.com>
2020-10-26 18:41:53 +0000

    Delete and Edit functionality

--- feedback/urls.py
+++ feedback/urls.py
@@ -1,9 +1,20 @@
 from django.urls import path
-from .views import FeedbackListView
+from .views import (
 
+    FeedbackListView,
+    FeedbackUpdateView,
+    FeedbackDetailView,
+    FeedbackDeleteView,
+)
 
 
 urlpatterns = [
 
+    path('<int:pk>/edit/', FeedbackUpdateView.as_view(), name='feedback_edit'),
+    path('<int:pk>/', FeedbackDetailView.as_view(), name='feedback_detail'),
+    path('<int:pk>/delete/', FeedbackDeleteView.as_view(), name='feedback_delete'),
     path('', FeedbackListView.as_view(), name='feedback_list'),
+
+
+
 ]
--- feedback/views.py
+++ feedback/views.py
@@ -1,10 +1,30 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import UpdateView, DeleteView
 from .models import Feedback
+from django.urls import reverse_lazy
 
 
 class FeedbackListView(ListView):
     model = Feedback
     template_name = 'feedback_list.html'
+
+
+class FeedbackUpdateView(UpdateView):
+    model = Feedback
+    fields = ('feedback',)
+    template_name = 'feedback_edit.html'
+
+
+class FeedbackDeleteView(DeleteView):
+    model = Feedback
+    template_name = 'feedback_delete.html'
+    success_url = reverse_lazy('feedback_list')
+
+
+class FeedbackDetailView(DetailView):
+    model = Feedback
+    template_name = 'feedback_detail.html'
+    
 
 # Create your views here.
